634a63270a71dcb450f9e85bd917ae1e90974d86
Marlon Vieira <124369161+MvsHub@users.noreply.github.com>
2025-03-15 19:12:59 +0000

    fix: Update theme-provider.tsx to fix type error

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,9 +1,13 @@
 "use client"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import type * as React from "react"
+
+export function ThemeProvider({
+  children,
+  ...props
+}: {
+  children: React.ReactNode
+} & Parameters<typeof NextThemesProvider>[0]) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
-
-
