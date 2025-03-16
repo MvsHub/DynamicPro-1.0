9e8980812ab34453d13e1fa710f4ca652cd692b5
Marlon Vieira <124369161+MvsHub@users.noreply.github.com>
2025-03-16 03:30:04 +0000

    Corrigir conflitos de merge no theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,25 +1,13 @@
 "use client"
-<<<<<<< HEAD
 
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type * as React from "react"
 
-=======
-
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type * as React from "react"
-
->>>>>>> 7efb6867513876d6299191c6e9cea57bb525a9ef
-export function ThemeProvider({
+export function ThemeSystem({
   children,
   ...props
 }: {
   children: React.ReactNode
-} & Parameters<typeof NextThemesProvider>[0]) {
+} & React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-<<<<<<< HEAD
 }
-=======
-}
-
->>>>>>> 7efb6867513876d6299191c6e9cea57bb525a9ef
