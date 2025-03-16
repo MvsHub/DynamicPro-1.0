c11222b0265b24275e57be6365c8a8d9f439eea6
Marlon Vieira <124369161+MvsHub@users.noreply.github.com>
2025-03-16 04:19:02 +0000

    Atualizar configuração do Tailwind CSS e dependências

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  content: [
+    "./pages/**/*.{ts,tsx,js,jsx,mdx}",
+    "./components/**/*.{ts,tsx,js,jsx,mdx}",
+    "./app/**/*.{ts,tsx,js,jsx,mdx}",
+    "./src/**/*.{ts,tsx,js,jsx,mdx}",
+    "*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -69,6 +75,7 @@ module.exports = {
   },
   plugins: [require("tailwindcss-animate")],
 }
+
 
 
   
